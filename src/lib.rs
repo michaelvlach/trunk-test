ecac1d0aa22b283eae208052d772d8771d7611ec
michaelvlach <vlach.michael@gmail.com>
2022-08-16 18:24:22 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -11,4 +11,10 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+
+    #[test]
+    fn it_works2() {
+        let result = add(2, 3);
+        assert_eq!(result, 5);
+    }
 }
